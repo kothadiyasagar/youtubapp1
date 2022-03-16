ec7c92ab1a794c0df77cbbe613c5b61b18e34cbb
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-03-16 14:53:31 +0530

    csss done

---
+++ index.js
@@ -0,0 +1,34 @@
+const searchvideos = async ()=>{
+    console.log("a")
+    try {
+        let inp = document.getElementById("search").value;
+        let res =await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${inp}&key=AIzaSyABQVG89PmKmLPZD2DulOIWVKcjcKXWmWg&maxResults=50`)
+   
+    let data = await res.json()
+    let videos=data.items
+    appendvideos(videos)
+     console.log("data:",data);
+} catch(error) {
+    console.log("erroe:",error)
+}
+};
+
+
+  const appendvideos =(data) => {
+   document.getElementById("content").innerHTML=""
+      try{
+          data.map((el) =>{
+              let div=document.createElement("div")
+              let title=document.createElement("p")
+              title.innerText=el.snippet.title
+              let iframe=document.createElement("img")
+              iframe.src=el.snippet.thumbnails.high.url;
+              iframe.style.width="100%"
+              iframe.style.height="170px"
+              div.append(iframe,title)
+              document.getElementById("content").append(div)
+          });
+      } catch(error){
+          console.log("error:",error)
+      }
+  }
